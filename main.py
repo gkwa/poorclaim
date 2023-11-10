4676a902e03af50c56dfaf4f01f3f67f7ed9a764
Taylor Monacelli <taylormonacelli@gmail.com>
2023-11-10 11:15:28 -0800

    save state

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import jinja2
 import neo4j
 import yaml
@@ -17,31 +19,52 @@ def limit_results(results, limit):
     return results[:limit] if limit else results
 
 
-driver = neo4j.GraphDatabase.driver(
-    # "bolt://localhost:7687", auth=("username", "password")
-    "bolt://localhost:7687",
-    auth=("", ""),
-)
+def report_titles(data):
+    titles = [(item["title"], len(item["title"])) for item in data]
+    titles.sort(key=lambda x: len(x[0]))
 
-# Load data from YAML into query_data
-with open("data.yaml", "r") as file:
-    query_data = yaml.load(file, Loader=yaml.FullLoader)
+    for title, length in titles:
+        print(f"{title} (Length: {length})")
 
-for item in query_data:
-    item["results"] = run_cypher_query(
-        driver,
-        item["cypher_query"],
-        limiter=lambda results: limit_results(results, item.get("limit")),
+
+def main():
+    parser = argparse.ArgumentParser(description="read/write data.yaml")
+    parser.add_argument("--titles", action="store_true", help="titles")
+    args = parser.parse_args()
+
+    driver = neo4j.GraphDatabase.driver(
+        # "bolt://localhost:7687", auth=("username", "password")
+        "bolt://localhost:7687",
+        auth=("", ""),
     )
 
-env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader("."),
-)
-template = env.get_template("template.org")
+    # Load data from YAML into query_data
+    with open("data.yaml", "r") as file:
+        query_data = yaml.load(file, Loader=yaml.FullLoader)
 
-rendered_manual = template.render(data=query_data)
+    if args.titles:
+        report_titles(query_data)
+        return
 
-with open("manual.org", "w") as manual_file:
-    manual_file.write(rendered_manual)
+    for item in query_data:
+        item["results"] = run_cypher_query(
+            driver,
+            item["cypher_query"],
+            limiter=lambda results: limit_results(results, item.get("limit")),
+        )
 
-driver.close()
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("."),
+    )
+    template = env.get_template("template.org")
+
+    rendered_manual = template.render(data=query_data)
+
+    with open("manual.org", "w") as manual_file:
+        manual_file.write(rendered_manual)
+
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
